9b90fd308c575fca0dab1564ed2050ce97db79c1
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-06-20 22:12:23 -0300

    first commit

---
+++ google.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import json
+
+elements = {
+    'url_google': 'https://www.google.com.br/',
+    'field': '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input'
+}
+
+def open_site(driver):
+    v_url = elements['url_google']
+    driver.get(v_url)   # carrega o site solicitado
+    driver.implicitly_wait(30) # aguarda por 30s o site carregar
+
+def search(driver):
+    v_field = driver.find_element_by_xpath(elements['field'])
+    v_field.send_keys("alter do chao")
+    sleep(2)
+    v_field.send_keys(Keys.ENTER)
+
+
+#--------------------------------------------------------------#
+#--------------- INICIALIZAÇÃO PRINCIPAL ----------------------#
+#--------------------------------------------------------------#
+def setup():
+    opt = Options()
+    driver = webdriver.Chrome(options = opt)
+    return driver
+
+def main():
+    driver = setup()
+    open_site(driver)
+    search(driver)
+    breakpoint()
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
+#--------------------------------------------------------------#
+
